4d599cff6e806ed890be46dcd3a2a1a5b2087872
Laynoll Diaz M <laynolldiaz@beondev.com>
2021-08-07 13:57:43 +0200

    MenuDishes table included

---
+++ src/Rest.DataBase/dbo/Tables/MenuDishes.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[MenuDishes] (
+    [IdMenu]     INT          NOT NULL,
+    [IdDish]     INT          NOT NULL,
+    [IdCategory] INT          NOT NULL,
+    [Price]      DECIMAL (18) NOT NULL,
+    [Id]         INT          IDENTITY (1, 1) NOT NULL,
+    CONSTRAINT [PK_MenuDishes] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_MenuDishes_Categories] FOREIGN KEY ([IdCategory]) REFERENCES [dbo].[Categories] ([Id]),
+    CONSTRAINT [FK_MenuDishes_Dishes] FOREIGN KEY ([IdDish]) REFERENCES [dbo].[Dishes] ([Id]),
+    CONSTRAINT [FK_MenuDishes_Menus] FOREIGN KEY ([IdMenu]) REFERENCES [dbo].[Menus] ([Id])
+);
+
